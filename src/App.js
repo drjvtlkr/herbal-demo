04a25f7cfa3d54dac3397ec0e625164606a313d9
Dheeraj Vithalkar <dheerajrvithalkar@gmail.com>
2023-10-03 11:58:14 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Home from './Components/Home'
 
 function App() {
-  return <div className="App">hELLO</div>
+  return (
+    <div className="App">
+      hELLO
+      <Home />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../Assets/Logo.svg'
+
+const Navbar = () => {
+  return (
+    <div>
+      {' '}
+      thisis Navbar
+      <Logo />
+    </div>
+  )
+}
+
+export default Navbar
