52223bdf26989d112c40e0276726faf04eb5cd70
Dheeraj Vithalkar <dheerajrvithalkar@gmail.com>
2023-10-03 16:21:55 +0530

    add: login, signup

---
+++ src/Components/Login&Signup/LoginForm.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+
+const LoginForm = (props) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [loginError, setLoginError] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/your-login-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert('Login successful');
+      } else {
+        setLoginError('Login failed. Please check your credentials.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white justify-center">
+              Log In Form
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Email Address
+                </label>
+                <input
+                  type="email" 
+                  name="email"
+                  id="email"
+                  value={formData.email} 
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder=""
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              {loginError && (
+                <div className="text-red-600 dark:text-red-400">{loginError}</div>
+              )}
+              <button
+                type="submit"
+                className="w-full text-gray-800 bg-primary-600 border border-gray-500 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 "
+              >
+                Log In
+              </button>
+              <p className="text-sm font-light c dark:text-gray-400">
+                Don’t have an account yet? 
+                <span> </span>
+                <button
+                  onClick={() => props.onFormSwitch('Signup')}
+                  className="text-blue-800 text-primary transition duration-150 ease-in-out hover:text-primary-800 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                >
+                  Signup
+                </button>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginForm;
---
+++ src/Components/Login&Signup/SignupForm.js
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+
+const SignupForm = (props) => {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    try {
+      const response = await fetch('/your-signup-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+    
+      if (response.ok) {
+        console.log('Registration successful');
+      } else {
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    
+
+    
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
+      <div className="bg-white dark:bg-gray-900 shadow-md rounded-md py-6 px-12 w-[2*96] sm:w-96">
+        <h2 className="text-2xl font-semibold mb-4">SignUp Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="fullName" className="text-gray-700 dark:text-gray-300">
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              id="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              className="block w-full mt-1 px-3 py-2.5 text-gray-800 dark:text-gray-100 border border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 rounded-md focus:outline-none focus:ring-blue-600"
+              placeholder=""
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="email" className="text-gray-700 dark:text-gray-300">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="block w-full mt-1 px-3 py-2.5 text-gray-800 dark:text-gray-100 border border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 rounded-md focus:outline-none focus:ring-blue-600"
+              placeholder=""
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="phoneNumber" className="text-gray-700 dark:text-gray-300">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name="phoneNumber"
+              id="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className="block w-full mt-1 px-3 py-2.5 text-gray-800 dark:text-gray-100 border border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 rounded-md focus:outline-none focus:ring-blue-600"
+              placeholder=""
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="text-gray-700 dark:text-gray-300">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="block w-full mt-1 px-3 py-2.5 text-gray-800 dark:text-gray-100 border border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 rounded-md focus:outline-none focus:ring-blue-600"
+              placeholder=""
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="confirmPassword" className="text-gray-700 dark:text-gray-300">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className="block w-full mt-1 px-3 py-2.5 text-gray-800 dark:text-gray-100 border border-gray-300 dark:border-gray-600 dark:focus:border-blue-500 rounded-md focus:outline-none focus:ring-blue-600"
+              placeholder=""
+              required
+            />
+          </div>
+
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full text-gray-800 bg-primary-600 border border-gray-300 hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-6 text-center text-neutral-800 dark:text-neutral-200">
+          Already have an account?{' '}
+          <button
+            onClick={() => props.onFormSwitch('login')}
+            className="text-blue-600 hover:text-blue-700 focus:text-blue-800 dark:text-blue-400 dark:hover:text-blue-500 dark:focus:text-blue-600"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
